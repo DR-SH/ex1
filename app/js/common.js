cecc2fe4fb82c6b1fe9060eacb5b2bd10c7a3bab
Andrey <a89n@bk.ru>
2019-01-22 02:37:20 +0300

    first version of layout done

---
+++ app/js/common.js
@@ -0,0 +1,299 @@
+$(function() {
+
+//	nav menu settings
+	$('ul .nav').smartmenus({
+		subMenusSubOffsetX: 1,
+		subMenusSubOffsetY: -8
+	});
+	
+//	big slider settings
+	
+	$('#home .owl-carousel').owlCarousel({
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplaySpeed: 1000,
+		navSpeed: 500,
+		dotsSpeed: 500,
+		items: 1,
+		nav: true,
+		navText: ["<i class='fa fa-angle-double-left'</i>",
+				"<i class='fa fa-angle-double-right'</i>"]
+	});
+	
+	// text position changing while hover
+	$('#home .inner-carousel').bind('mouseover', function(e){
+		$(this).stop().animate({'background-position-x': '55%', 'background-position-y': '55%'}, 2000);
+	});
+	$('#home .inner-carousel').bind('mouseout', function(e){
+		$(this).stop().animate({'background-position-x': '50%', 'background-position-y': '50%'}, 2000);
+	});	
+	
+	// 
+	$('#home .owl-carousel .active').find('.inner-carousel-text').css('display', 'block');
+	
+	$('#home .owl-carousel').on('changed.owl.carousel', function(event) {
+		let item      = event.item.index;     // Position of the current item		
+		let current = $(this).find('.owl-item:eq('+item+')');
+		current.siblings().find('.inner-carousel-text').hide();
+		current.find('.inner-carousel-text').slideDown(1000);	
+	});
+	
+
+//	services section
+	//rotating icon & scale
+	$('.services-bg').bind('mouseenter', function(e){
+		let icon = $(this).find('.services-icon');
+		let rotat = 0;
+		if(typeof icon.attr('style') !== "undefined"){
+			rotat = icon.attr('style').replace(/[^-\d\.]/g, '');
+		}
+		rotat=parseInt(rotat);
+		rotat+=360;
+		icon.attr('style', 'transform: rotate('+rotat+'deg)');
+		$(this).attr('style', 'transform: scale(1.05)');
+	});
+	
+	
+	$('.services-bg').bind('mouseleave', function(e){
+		$(this).attr('style', 'transform: scale(1)');
+	});
+	
+// #services carousel
+
+	$('#services .owl-carousel').owlCarousel({
+		loop: false,
+		autoplay: false,
+		nav: false,
+		margin: 30,
+		responsive:{ //adaptation to screen mode
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			992:{
+				items:3
+			},
+			1200:{
+				items:4
+			}			
+		}	
+	});
+	
+ // #about 
+ 
+ 	$('#about .owl-carousel').owlCarousel({
+		loop: true,
+		autoplay: false,
+		autoplayTimeout: 3000,	
+		autoplaySpeed: 1000,	
+		slideTransition: 'ease',	
+		nav: false,
+		margin: 0,
+		slideBy: 1,
+		center: true,
+		responsive:{ //adaptation to screen mode
+			0:{
+				items:1
+			},
+			768:{
+				items:3
+			},
+
+		}	
+	});
+	
+	//плавное уменьшение последнего элемента в карусели #about
+	
+	$('#about .owl-carousel').on('changed.owl.carousel', function(event) {
+		let pages  = event.page.count; 
+		let page  = event.page.index;     
+		console.log(pages - page);	
+		let current = $(this).find('.owl-item:eq(2)');
+		if((pages-page) == 1){			
+			current.addClass('cur');
+		}
+		else{
+			current.removeClass('cur');
+		}
+	});	
+
+		/////
+
+///// donuts
+	$("#halfcircle1").circliful({
+		percent: 50,
+		animateInView: true,
+		animation: .4,
+        animationStep: 2,        
+        backgroundBorderWidth: 20,
+		foregroundBorderWidth: 20,
+		halfCircle: 1,
+		backgroundColor: '#76c7c0',
+		foregroundColor: '#e2534b',
+		noPercentageSign: true,
+		fontColor: '#7f8c8c',
+		text: 'Suspendisse',
+		percentageTextSize: 34,
+		textStyle: 'font-size: 20px',
+		textColor: '#7f8c8c',
+		textY: 150
+    });
+	$("#halfcircle2").circliful({
+		percent: 70,
+		animateInView: true,
+		animation: .4,
+        animationStep: 2,        
+        backgroundBorderWidth: 20,
+		foregroundBorderWidth: 20,
+		halfCircle: 1,
+        //pointSize: 50,
+		backgroundColor: '#76c7c0',
+		foregroundColor: '#e2534b',
+		noPercentageSign: true,
+		fontColor: '#7f8c8c',
+		text: 'Maecenas',
+		percentageTextSize: 34,
+		textStyle: 'font-size: 20px',
+		textColor: '#7f8c8c',
+		textY: 150
+	});		
+	$("#halfcircle3").circliful({
+		percent: 80,
+		animateInView: true,
+		animation: 1,
+        animationStep: 4,        
+        backgroundBorderWidth: 20,
+		foregroundBorderWidth: 20,
+		halfCircle: 1,
+		backgroundColor: '#76c7c0',
+		foregroundColor: '#e2534b',
+		noPercentageSign: true,
+		fontColor: '#7f8c8c',
+		text: 'Aliquam',
+		percentageTextSize: 34,
+		textStyle: 'font-size: 20px',
+		textColor: '#7f8c8c',
+		textY: 150
+	});		
+	$("#halfcircle4").circliful({
+		percent: 100,
+		animateInView: true,
+		animation: 1,
+        animationStep: 2,        
+        backgroundBorderWidth: 20,
+		foregroundBorderWidth: 20,
+		halfCircle: 1,
+		backgroundColor: '#76c7c0',
+		foregroundColor: '#e2534b',
+		noPercentageSign: true,
+		fontColor: '#7f8c8c',
+		text: 'Habitasse',
+		percentageTextSize: 34,
+		textStyle: 'font-size: 20px',
+		textColor: '#7f8c8c',
+		textY: 150		
+	});		
+
+///// user carousel and range-slider
+
+
+	let rOwl = $('#clients-carousel .owl-carousel');
+	
+	let rOwlAmount = rOwl.find('.inner-carousel').length;  //общее количество элементов в карусели
+	
+	 // определение количества элементов на одной странице в карусели в зависимостии от ширины экрана
+	 
+	let res = function(){  
+		if($( window ).width() > 768){
+			return 6;
+		}
+		else{
+			return 3 ;
+		}	
+	};	
+	
+	let winWid = res(); // winWid - текущее кол-во эл-тов на странице
+	
+	$( window ).resize(function(){
+		winWid = res();
+/* 		let range = $(".js-range-slider").data("ionRangeSlider");
+		range.update({
+			max: Math.ceil(rOwlAmount/winWid)
+		}); */
+	});		
+	
+	// carousel
+	
+ 	rOwl.owlCarousel({
+		loop: true,
+		autoplay: false,
+		autoplayTimeout: 3000,	
+		autoplaySpeed: 1000,	
+		slideTransition: 'ease',	
+		nav: true,
+		dots: false,
+		margin: 30,
+		navText: ["<span><i class='fa fa-angle-left'</i></span>",
+				"<span><i class='fa fa-angle-right'</i></span>"],
+		responsive:{
+			0:{
+				items:3
+			},
+			768:{
+				items:6
+			},
+		}	
+	});	
+	
+	//range
+	let flag = true;
+    $(".js-range-slider").ionRangeSlider({
+        type: "single",
+		skin: "round",
+        min: 1,
+        //max: Math.ceil(rOwlAmount/winWid),
+		max: rOwlAmount,
+		from: 1,
+        grid: false,
+		hide_min_max: true,
+		hide_from_to: true,
+		onChange: function (data) {  // range callback
+			flag = false;
+			//rOwl.trigger('to.owl.carousel', (data.from-1)*winWid + 1);
+			console.log('666');
+			rOwl.trigger('to.owl.carousel', data.from-1, true);
+		},		
+		onFinish: function () {  // range callback
+			flag = true;
+		}			
+    });		
+	
+	
+	//changing range after carousel changed
+	
+	let changeRange = function(){
+		rOwl.on('changed.owl.carousel', function(event) {
+			if (!flag) return;
+			let item0  = event.item.index;
+			let item = item0 + 1 - rOwlAmount/2;	
+			if (item<1){
+				item = rOwlAmount;
+			}		
+			let currentPage = Math.ceil(item/winWid);
+			//console.log(item0 + ' ' + item + ' ' + winWid + ' ' + currentPage);	
+			let range = $(".js-range-slider").data("ionRangeSlider");
+			range.update({
+				//from: currentPage
+				from: item
+			}); 
+		});
+	};	
+	
+	$('#clients-carousel').find('.owl-nav').bind('hover', changeRange());
+	
+
+});
+
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var syntax        = 'sass'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require('gulp-notify'),
+		rsync         = require('gulp-rsync'),
+		imagemin   	  = require('gulp-imagemin'),
+		cache         = require('gulp-cache'),
+		del           = require('del');
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/bootstrap/bootstrap.bundle.min.js',	
+		'app/libs/smartmenus/jquery.smartmenus.min.js',  // nav menue
+		'app/libs/smartmenus/jquery.smartmenus.bootstrap-4.min.js',  //nav menue bootstrap addon
+		'app/libs/owlcarousel/owl.carousel.min.js',   //big slider and carousel
+		'app/libs/circliful/jquery.circliful.min.js',   //donuts
+		'app/libs/rangeslider/ion.rangeSlider.min.js'   //range slider
+		])
+	.pipe(concat('libs.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Includes files to deploy
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+ gulp.task('clean', function(){
+	return del.sync('dist');
+}); 
+
+gulp.task('watch', function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'))
+});
+
+gulp.task('default', gulp.parallel('watch', 'styles', 'scripts', 'browser-sync'));
+
+//////build
+gulp.task('construct', function(){
+	let buildJS = gulp.src(['app/js/libs.min.js', 'app/js/common.js'])
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+	
+	let buildCSS = gulp.src(['app/css/lib.min.css', 'app/css/main.min.css'])
+	.pipe(gulp.dest('dist/css'));
+	
+	let buildFonts =  gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'));
+	
+	let buildImg = gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
+	.pipe(cache(imagemin({interlaced: true})))
+	.pipe(gulp.dest('dist/img')); 
+	
+	let buildHtml =  gulp.src('app/*.html')
+	.pipe(gulp.dest('dist'));	
+	return 1;
+	
+});
+
+gulp.task('build', gulp.parallel('clean', 'construct'));
